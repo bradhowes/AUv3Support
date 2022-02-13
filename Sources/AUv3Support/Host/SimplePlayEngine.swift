33e99645027a2de51c3a5ec44b822d8efc9cda5d
Brad Howes <bradhowes@mac.com>
2022-02-13 21:23:55 +0100

    Fixed broken audio chain.

--- Sources/AUv3Support/Host/SimplePlayEngine.swift
+++ Sources/AUv3Support/Host/SimplePlayEngine.swift
@@ -37,18 +37,34 @@ public final class SimplePlayEngine {
   }
 
   /**
-   Create new audio processing setup, with an audio file player connected directly to the mixer for the main
-   output device.
+   Create new audio processing setup, with an audio file player for a signal source.
    */
   public init(name: String, audioFileName: String) {
     self.log = .init(subsystem: name, category: "SimplePlayEngine")
     self.file = Self.audioFileResource(name: audioFileName)
     engine.attach(player)
-    engine.connect(player, to: engine.mainMixerNode, format: file.processingFormat)
   }
 }
 
 extension SimplePlayEngine {
+
+  /**
+   Install an effect AudioUnit between an audio source and the main output mixer.
+
+   @param audioUnit the audio unit to install
+   @param completion closure to call when finished
+   */
+  public func connectEffect(audioUnit: AVAudioUnit, completion: @escaping (() -> Void) = {}) {
+    os_log(.debug, log: log, "connectEffect BEGIN")
+    defer { completion() }
+    os_log(.debug, log: log, "connectEffect - attaching effect")
+    engine.attach(audioUnit)
+    os_log(.debug, log: log, "connectEffect - player -> effect")
+    engine.connect(player, to: audioUnit, format: file.processingFormat)
+    os_log(.debug, log: log, "connectEffect - effect -> mixer (speaker)")
+    engine.connect(audioUnit, to: engine.mainMixerNode, format: file.processingFormat)
+    os_log(.debug, log: log, "connectEffect END")
+  }
 
   /**
    Start playback of the audio file player.
@@ -96,67 +112,9 @@ extension SimplePlayEngine {
     if player.isPlaying { stop() } else { start() }
     return player.isPlaying
   }
-
-  /**
-   Install an effect AudioUnit between an audio source and the main output mixer. If there was a previous effect
-   installed it is removed from the signal chain. If the sound resource was playing it will be resumed after the
-   new effect is installed.
-
-   @param audioUnit the audio unit to install
-   @param completion closure to call when finished
-   */
-  public func connectEffect(audioUnit: AVAudioUnit, completion: @escaping (() -> Void) = {}) {
-    os_log(.debug, log: log, "connectEffect BEGIN")
-    defer { completion() }
-    pauseWhile {
-      disconnectEffect()
-      activeEffect = audioUnit
-      os_log(.debug, log: log, "connectEffect - attaching effect")
-      engine.attach(audioUnit)
-      os_log(.debug, log: log, "connectEffect - connecting player to effect - format: %{public}s",
-             file.processingFormat.description)
-      engine.connect(audioUnit, to: engine.mainMixerNode, format: file.processingFormat)
-      os_log(.debug, log: log, "connectEffect - connecting effect to mixer")
-      // engine.connect(audioUnit, to: engine.mainMixerNode, format: file.processingFormat)
-      engine.connect(audioUnit, to: engine.mainMixerNode, format: file.processingFormat)
-    }
-    os_log(.debug, log: log, "connectEffect END")
-  }
-
-  /**
-   Uninstall a previously-installed effect AudioUnit.
-   */
-  public func disconnectEffect() {
-    os_log(.debug, log: log, "disconnectEffect BEGIN")
-    guard let previous = activeEffect else { return }
-    activeEffect = nil
-    pauseWhile {
-      os_log(.debug, log: log, "disconnectEffect - disconnecting effect")
-      engine.disconnectNodeInput(previous)
-      engine.disconnectNodeInput(engine.mainMixerNode)
-      os_log(.debug, log: log, "disconnectEffect - detaching effect")
-      engine.detach(previous)
-      os_log(.debug, log: log, "disconnectEffect - connecting player to mixer")
-      engine.connect(player, to: engine.mainMixerNode, format: file.processingFormat)
-    }
-    os_log(.debug, log: log, "disconnectEffect END")
-  }
 }
 
 private extension SimplePlayEngine {
-
-  /**
-   If player is currently playing audio pause it the execution of the given block and then resume it after the block
-   is done.
-
-   - parameter block: closure to execute while player is paused
-   */
-  private func pauseWhile(_ block: () -> Void) {
-    let wasPlaying = player.isPlaying
-    if wasPlaying { player.pause() }
-    block()
-    if wasPlaying { player.play() }
-  }
 
   private func updateAudioSession(active: Bool) {
     #if os(iOS)
