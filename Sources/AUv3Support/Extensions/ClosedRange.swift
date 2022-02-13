86215d432a00e75eba66361ff56473b2ff9cef0b
Brad Howes <bradhowes@mac.com>
2022-02-13 11:57:31 +0100

    Added span property

--- Sources/AUv3Support/Extensions/ClosedRange.swift
+++ Sources/AUv3Support/Extensions/ClosedRange.swift
@@ -2,6 +2,9 @@
 
 public extension ClosedRange where Bound: BinaryFloatingPoint {
 
+  /// Obtain the difference between the upper bound and lower bound
+  var span: Bound { upperBound - lowerBound }
+
   /// Obtain the value that lies between the bounds of the range.
-  var mid: Bound { (upperBound - lowerBound) / 2.0 + lowerBound }
+  var mid: Bound { span / 2.0 + lowerBound }
 }
