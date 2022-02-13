5ff20bd967bbe4063a5ce31b6a78906bcf6f5877
Brad Howes <bradhowes@mac.com>
2022-02-13 12:00:36 +0100

    Changed permissions.

--- Sources/AUv3Support/Extensions/ClosedRange.swift
+++ Sources/AUv3Support/Extensions/ClosedRange.swift
@@ -1,10 +1,10 @@
 // Copyright © 2022 Brad Howes. All rights reserved.
 
-public extension ClosedRange where Bound: BinaryFloatingPoint {
+extension ClosedRange where Bound: BinaryFloatingPoint {
 
   /// Obtain the difference between the upper bound and lower bound
-  var span: Bound { upperBound - lowerBound }
+  public var span: Bound { upperBound - lowerBound }
 
   /// Obtain the value that lies between the bounds of the range.
-  var mid: Bound { span / 2.0 + lowerBound }
+  public var mid: Bound { span / 2.0 + lowerBound }
 }
