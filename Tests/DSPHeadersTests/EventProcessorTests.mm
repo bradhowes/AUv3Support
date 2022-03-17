eb6cb4cd4e671d66a5c4aaee6c933fec8652ddfb
Brad Howes <bradhowes@mac.com>
2022-03-17 10:20:07 +0100

    Added EventProcessor test

--- Tests/DSPHeadersTests/EventProcessorTests.mm
+++ Tests/DSPHeadersTests/EventProcessorTests.mm
@@ -11,6 +11,9 @@ using namespace DSPHeaders;
 struct MockEffect : public EventProcessor<MockEffect>
 {
   MockEffect() : EventProcessor<MockEffect>("Foo") {}
+  void setParameterFromEvent(const AUParameterEvent&) {}
+  void doMIDIEvent(AUMIDIEvent) {}
+  void doRendering(NSInteger outputBusNumber, BusBuffers, BusBuffers, AUAudioFrameCount) {}
 };
 
 @interface EventProcessorTests : XCTestCase
@@ -54,9 +57,30 @@ struct MockEffect : public EventProcessor<MockEffect>
   AudioTimeStamp timestamp = AudioTimeStamp();
   AudioUnitRenderActionFlags flags = 0;
 
+  AUAudioUnitStatus (^mockPullInput)(AudioUnitRenderActionFlags *actionFlags, const AudioTimeStamp *timestamp,
+                                     AUAudioFrameCount frameCount, NSInteger inputBusNumber,
+                                     AudioBufferList *inputData);
+  mockPullInput = ^(AudioUnitRenderActionFlags *actionFlags, const AudioTimeStamp *timestamp,
+                    AUAudioFrameCount frameCount, NSInteger inputBusNumber, AudioBufferList *inputData) {
+    auto bufferCount = inputData->mNumberBuffers;
+    for (int index = 0; index < bufferCount; ++index) {
+      auto& buffer = inputData->mBuffers[index];
+      auto numberOfChannels = buffer.mNumberChannels;
+      auto bufferSize = buffer.mDataByteSize;
+      assert(sizeof(AUValue) * frameCount == bufferSize);
+      auto ptr = reinterpret_cast<AUValue*>(buffer.mData);
+      for (int pos = 0; pos < bufferSize; ++pos) {
+        ptr[pos] = pos;
+      }
+    }
+
+    return 0;
+  };
+
   AUAudioFrameCount frames = 4;
   NSInteger bus = 0;
-  // auto status = effect.processAndRender(&timestamp, frames, bus, [buffer mutableAudioBufferList], nil, pullInputBlock);
+  auto status = effect.processAndRender(&timestamp, frames, bus, [buffer mutableAudioBufferList], nil, mockPullInput);
+  XCTAssertEqual(status, 0);
 }
 
 @end
