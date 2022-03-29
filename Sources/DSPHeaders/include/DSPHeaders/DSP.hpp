eb385207ffb6f31a1a94d8af60f833e8e62559a4
Brad Howes <bradhowes@mac.com>
2022-03-29 12:42:18 +0200

    Apply constexpr attribute where applicable.

--- Sources/DSPHeaders/include/DSPHeaders/DSP.hpp
+++ Sources/DSPHeaders/include/DSPHeaders/DSP.hpp
@@ -14,7 +14,7 @@ namespace DSPHeaders::DSP {
  @param modulator the value to translate
  @returns value in range [-1, +1]
  */
-template <typename T> auto unipolarToBipolar(T modulator) noexcept { return 2.0 * modulator - 1.0; }
+template <typename T> constexpr auto unipolarToBipolar(T modulator) noexcept { return 2.0 * modulator - 1.0; }
 
 /**
  Translate value in range [-1, +1] into one in range [0, +1]
@@ -22,7 +22,7 @@ template <typename T> auto unipolarToBipolar(T modulator) noexcept { return 2.0 
  @param modulator the value to translate
  @returns value in range [0, +1]
  */
-template <typename T> auto bipolarToUnipolar(T modulator) noexcept { return 0.5 * modulator + 0.5; }
+template <typename T> constexpr auto bipolarToUnipolar(T modulator) noexcept { return 0.5 * modulator + 0.5; }
 
 /**
  Perform linear translation from a value in range [0.0, 1.0] into one in [minValue, maxValue].
@@ -32,7 +32,7 @@ template <typename T> auto bipolarToUnipolar(T modulator) noexcept { return 0.5 
  @param maxValue the highest value to return when modulator is +1
  @returns value in range [minValue, maxValue]
  */
-template <typename T> auto unipolarModulation(T modulator, T minValue, T maxValue) noexcept {
+template <typename T> constexpr auto unipolarModulation(T modulator, T minValue, T maxValue) noexcept {
   return std::clamp<T>(modulator, 0.0, 1.0) * (maxValue - minValue) + minValue;
 }
 
@@ -44,7 +44,7 @@ template <typename T> auto unipolarModulation(T modulator, T minValue, T maxValu
  @param maxValue the highest value to return when modulator is +1
  @returns value in range [minValue, maxValue]
  */
-template <typename T> auto bipolarModulation(T modulator, T minValue, T maxValue) noexcept {
+template <typename T> constexpr auto bipolarModulation(T modulator, T minValue, T maxValue) noexcept {
   auto mid = (maxValue - minValue) * 0.5;
   return std::clamp<T>(modulator, -1.0, 1.0) * mid + mid + minValue;
 }
@@ -58,7 +58,7 @@ template <typename T> auto bipolarModulation(T modulator, T minValue, T maxValue
  @param angle value between -PI and PI
  @returns approximate sin value
  */
-template <typename T> auto parabolicSine(T angle) noexcept {
+template <typename T> constexpr auto parabolicSine(T angle) noexcept {
   const T B = 4.0 / M_PI;
   const T C = -4.0 / (M_PI * M_PI);
   const T P = 0.225;
@@ -96,7 +96,7 @@ struct Cubic4thOrder {
  @param x2 third value to use
  @param x3 fourth value to use
  */
-inline static double cubic4thOrder(double partial, double x0, double x1, double x2, double x3) noexcept {
+inline constexpr double cubic4thOrder(double partial, double x0, double x1, double x2, double x3) noexcept {
   // Partial is expected to be < 1.0 so with truncation index should always be in range [0, 1)
   size_t index = size_t(partial * Cubic4thOrder::TableSize);
   assert(index < Cubic4thOrder::TableSize);
