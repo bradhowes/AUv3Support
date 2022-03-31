b9e19fa095c7d096b967c7f100389886c6feb5f7
Brad Howes <bradhowes@mac.com>
2022-03-31 19:30:18 +0200

    Commentary

--- Sources/AUv3Support/Parameters/AUParameterHandler.swift
+++ Sources/AUv3Support/Parameters/AUParameterHandler.swift
@@ -1,18 +1,23 @@
 import CoreAudioKit
 
 /**
- Protocol for entities that can respond to get/set requests in the AUParameterTree. The Adapter will conform to this
- protocol, but the C++ kernel will actually handle the requests.
+ Protocol for entities that can respond to get/set requests in the AUParameterTree.
  */
 @objc public protocol AUParameterHandler {
 
   /**
    Set an AUParameter to a new value
+
+   - parameter parameter: the AUParameter to update
+   - parameter value: the value to store
    */
   func set(_ parameter: AUParameter, value: AUValue)
 
   /**
    Get the current value of an AUParameter
+
+   - parameter parameter: the AUParameter to query
+   - returns the current value
    */
   func get(_ parameter: AUParameter) -> AUValue
 }
