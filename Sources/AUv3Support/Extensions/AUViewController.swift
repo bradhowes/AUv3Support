e009a4c1aef0c57e3057cea4b59764e17870b12c
Brad Howes <bradhowes@mac.com>
2022-04-07 18:42:34 +0200

    Refinement

--- Sources/AUv3Support/Extensions/AUViewController.swift
+++ Sources/AUv3Support/Extensions/AUViewController.swift
@@ -4,13 +4,18 @@ import CoreAudioKit
 
 public extension AUViewController {
 
-  func useAudioUnit(_ audioUnit: FilterAudioUnit, editors: [AUParameterEditor]) -> NSKeyValueObservation {
+  /**
+   Install an observer on `currentPreset` attribute of a FilterAudioUnit so that the editors are updated when the
+   preset value changes.
+
+   - parameter audioUnit: the FilterAudioUnit to monitor
+   - parameter editors: the collection of AUParameterEditor values to update
+   - returns: token for the observation
+   */
+  static func updateEditorsOnPresetChange(_ audioUnit: FilterAudioUnit,
+                                          editors: [AUParameterEditor]) -> NSKeyValueObservation {
     return audioUnit.observe(\.currentPreset) { _, _ in
-      DispatchQueue.main.async {
-        for editor in editors {
-          editor.updateControl()
-        }
-      }
+      DispatchQueue.main.async { editors.forEach { $0.updateControl() }
     }
   }
 }
