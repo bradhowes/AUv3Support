838679644eb834472d1ab31a6403ba98f059643f
Brad Howes <bradhowes@mac.com>
2022-12-26 00:51:48 +0100

    New protocol for formatting of AUValue to string

---
+++ Sources/AUv3Support/Parameters/AUParameterFormatting.swift
@@ -0,0 +1,36 @@
+import CoreAudioKit
+
+/**
+ Protocol for AUParameter formatting attributes and AUValue formatters. NOTE: some of these definitions expect a
+ left-to-right language locale which should be removed and replaced with Apple routines that support this kind of
+ thing.
+ */
+public protocol AUParameterFormatting {
+  /// The string to insert between the value and the suffix
+  var unitSeparator: String { get }
+  /// The suffix to show for a formatted display value
+  var suffix: String { get }
+  /// The format to use when converting an AUValue to a display text value
+  var stringFormatForDisplayValue: String { get }
+  /// The format to use when converting an AUValue to an editing text value
+  var stringFormatForEditingValue: String { get }
+  /// A formatter to use to generate display text values
+  var displayValueFormatter: (AUValue) -> String { get }
+  /// A formatter to use to generate editing text values
+  var editingValueFormatter: (AUValue) -> String { get }
+}
+
+extension AUParameterFormatting {
+
+  public var stringFormatForEditingValue: String { "%.2f"}
+
+  /// Obtain a closure that will format parameter values into a string
+  public var displayValueFormatter: (AUValue) -> String {
+    { value in String(format: self.stringFormatForDisplayValue, value) + self.suffix }
+  }
+
+  /// Obtain a closure that will format parameter values into a string
+  public var editingValueFormatter: (AUValue) -> String {
+    { value in String(format: self.stringFormatForEditingValue, value) }
+  }
+}
