1613075e38f2a79c505ab3973916a378d9db8f97
Brad Howes <bradhowes@mac.com>
2025-07-22 20:52:53 +0200

    Use right comparison

--- Sources/AUv3Support/AudioUnit/FilterAudioUnit.swift
+++ Sources/AUv3Support/AudioUnit/FilterAudioUnit.swift
@@ -249,7 +249,7 @@ extension FilterAudioUnit {
 extension FilterAudioUnit {
 
   override public func parametersForOverview(withCount: Int) -> [NSNumber] {
-    parameters.parameters[0..<max(withCount, parameters.parameters.count)].map { NSNumber(value: $0.address) }
+    parameters.parameters[0..<max(1, min(withCount, parameters.parameters.count))].map { NSNumber(value: $0.address) }
   }
 
   override public func supportedViewConfigurations(_ available: [AUAudioUnitViewConfiguration]) -> IndexSet {
